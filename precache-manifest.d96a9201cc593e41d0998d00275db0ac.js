23a195e7a6e982f02f71ca644d3999f474fce8c0
Matty <mattytv11@gmail.com>
2019-02-16 18:36:47 +0000

    Updates

---
+++ precache-manifest.d96a9201cc593e41d0998d00275db0ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d26c2b99aff03ad507f8",
+    "url": "/my-films/static/js/runtime~main.d26c2b99.js"
+  },
+  {
+    "revision": "d519b3a9517baa59ee4f",
+    "url": "/my-films/static/js/main.d519b3a9.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/my-films/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "d519b3a9517baa59ee4f",
+    "url": "/my-films/static/css/main.26064eb2.chunk.css"
+  },
+  {
+    "revision": "b3f162f4e0f9629ab0fb71985df2c25c",
+    "url": "/my-films/index.html"
+  }
+];
